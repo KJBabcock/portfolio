27659500fc88a88af885a7e21b79b647c4c24ec4
SquatchTang <kyjaba@gmail.com>
2022-10-24 21:48:43 -0500

    Hero Images Set up

--- portfolio/src/routes/Contact.js
+++ portfolio/src/routes/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import HeroImg2 from '../components/HeroImg2'
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="CONTACT." text="Feel free to reach out for any reason!" />
       <Footer />
     </div>
   )
